5b5ff4a3d04361c36e6efe2dbbb4f4b5281806d7
xbb <xbbhdu@163.com>
2015-06-01 06:55:24 +0000

    Verson1

---
+++ src/ChatClient.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+
+public class ChatClient extends Frame
+{
+
+	public static void main(String[] args) 
+	{
+		new ChatClient().launchFrame();
+	}
+	public void launchFrame()
+	{
+		this.setLocation(100,100);
+		this.setSize(300, 300);
+		this.setVisible(true);
+		this.setBackground(Color.blue);
+	}
+}
